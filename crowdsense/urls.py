25f734f1fdc12d38c2ae7e16e8fd46086e1604c1
Maciej Pasternacki <maciej@pasternacki.net>
2009-07-23 03:21:41 +0200

    Underscore *is* allowed in slugs - change special char to a dot.

--- crowdsense/urls.py
+++ crowdsense/urls.py
@@ -6,8 +6,7 @@ urlpatterns = patterns(
     '',
     url(r'^(?P<slug>[-\w]+)/$', views.tracker_main, name='tracker_main'),
     url(r'^(?P<slug>[-\w]+)/(?P<channel_slug>[-\w]+)/$', views.tracker_channel, name='tracker_channel'),
-
-     # _ character does not occur in slugs, we use it in special urls
-    url(r'^__create__/$', views.tracker_create, name='tracker_create'),
-    url(r'^__edit__/(?P<slug>[-\w]+)/$', views.tracker_edit, name='tracker_edit'),
+    # dot character does not occur in slugs, we use it in special urls
+    url(r'^.create/$', views.tracker_create, name='tracker_create'),
+    url(r'^.edit/(?P<slug>[-\w]+)/$', views.tracker_edit, name='tracker_edit'),
     )
