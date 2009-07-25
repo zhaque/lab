429f4e94d4c68d81de2804f915ba41703e5f9a65
Maciej Pasternacki <maciej@pasternacki.net>
2009-07-25 03:36:06 +0200

    Bump muaccounts for nullable owner and redirection to settings page after creating muaccount.
    Drop requirement for username in CSV import; imported sites are now created with NULL owner.

--- crowdsense/management/commands/importcsv.py
+++ crowdsense/management/commands/importcsv.py
@@ -2,7 +2,6 @@ import csv
 import sys
 
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 from django.db import transaction
 from django.template.defaultfilters import slugify
 
@@ -10,7 +9,7 @@ from muaccounts.models import MUAccount
 import muaccounts.themes
 from crowdsense.models import Tracker
 
-LEGEND = ['owner', 'name', 'subdomain', 'theme', 'tracker_name', 'query']
+LEGEND = ['name', 'subdomain', 'theme', 'tracker_name', 'query']
 
 class Command(BaseCommand):
     help = 'Import CrowdSense accounts from CSV file'
@@ -34,34 +33,30 @@ class Command(BaseCommand):
         ln = 1
         def _warn(msg, type='Warning'):
             print >>sys.stderr, '%s:%d: %s: %s' % ( filename, ln, type, msg )
-        for owner, name, subdomain, theme, tracker_name, query in reader:
+        for name, subdomain, theme, tracker_name, query in reader:
             try:
                 ln += 1
-                if owner and name:
-                    owner, created = User.objects.get_or_create(username=owner)
-                    if not created:
-                        _warn('user %s existed' % owner)
-
+                if name:
                     themedict = muaccounts.themes.DEFAULT_THEME_DICT.copy()
                     for themespec in theme.strip().split('|'):
-                        k, v = themespec.split(':',1)
-                        if k in themedict:
-                            if v in muaccounts.themes.THEMES_DICT[k][1]:
-                                themedict[k]=v
+                        if themespec:
+                            k, v = themespec.split(':',1)
+                            if k in themedict:
+                                if v in muaccounts.themes.THEMES_DICT[k][1]:
+                                    themedict[k]=v
+                                else:
+                                    _warn('Unknown theme value %s' % themespec)
                             else:
-                                _warn('Unknown theme value %s' % themespec)
-                        else:
-                            _warn('Unknown theme key %s' % themespec)
+                                _warn('Unknown theme key %s' % themespec)
 
                     mua = MUAccount.objects.create(
-                        owner=owner,
                         name=name,
                         subdomain=subdomain or slugify(name),
                         is_public=True,
                         theme=themedict,
                         )
-                elif owner or name or subdomain or theme:
-                    raise ValueError("At least owner and name are required for account creation.")
+                elif subdomain or theme:
+                    raise ValueError("At least the plan name is required for account creation.")
                 if not mua:
                     _warn('skipped tracker')
                     continue
