108341305112baa038df3978e5b145a839e8c163
Maciej Pasternacki <maciej@pasternacki.net>
2009-07-29 01:27:10 +0200

    Silence django_pipes debug messages.

--- crowdsense/__init__.py
+++ crowdsense/__init__.py
@@ -6,3 +6,7 @@ try:
     settings.BING_APP_ID
 except:
     raise ImproperlyConfigured("BING_APP_ID setting not found.")
+
+# monkey-patch silence into django-pipes
+import django_pipes.main
+django_pipes.main._log = lambda msg: None
